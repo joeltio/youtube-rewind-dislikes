dacadac00e796cd97acd8bce4d4909cc8de54163
Joel Tio <joeltiojinhon@gmail.com>
2019-01-08 19:14:31 +0800

    Add file to get dislikes

---
+++ src/youtube.py
@@ -0,0 +1,17 @@
+import requests
+
+
+BASE_URL = "https://www.googleapis.com/youtube/v3"
+
+
+def get_dislikes(api_key, video_id):
+    url = f"{BASE_URL}/videos"
+
+    payload = {
+        "key": api_key,
+        "part": ["statistics"],
+        "id": video_id,
+    }
+
+    response = requests.get(url, params=payload)
+    return response.json()["items"][0]["statistics"]["dislikeCount"]
