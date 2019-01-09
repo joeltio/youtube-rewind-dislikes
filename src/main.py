7bb77fbc50664df1e7f10d0ed7272f3be8fe6c02
Joel Tio <joeltiojinhon@gmail.com>
2019-01-09 17:21:55 +0800

    Add main turtle loop

---
+++ src/main.py
@@ -0,0 +1,82 @@
+import turtle as t
+import datetime
+import time
+
+import graph
+import youtube
+import settings
+
+
+class FixedQueue:
+    def __init__(self, size):
+        self.size = size
+        self.queue = []
+
+    def __str__(self):
+        return f"FixedQueue({self.queue})"
+
+    def __repr__(self):
+        return f"FixedQueue({self.queue})"
+
+    def enqueue(self, element):
+        if len(self.queue) == self.size:
+            self.dequeue()
+
+        self.queue.append(element)
+
+    def dequeue(self):
+        return self.queue.pop(0)
+
+
+def create_fullscreen_window():
+    window = t.Screen()
+    window.screensize()
+    window.setup(height=1.0, width=1.0)
+
+    height = window.window_height()
+    width = window.window_width()
+
+    window.setworldcoordinates(0, height, width, 0)
+
+    return window
+
+
+def get_api_key(filepath):
+    f = open(filepath, "r")
+    return f.readline()
+
+
+def main():
+    window = create_fullscreen_window()
+    turtle = t.Turtle()
+    turtle.speed(0)
+
+    data = {
+        "data_points": FixedQueue(settings.GRAPH_DATA_LIMIT),
+        "time_points": FixedQueue(settings.GRAPH_DATA_LIMIT),
+    }
+
+    api_key = get_api_key(settings.API_KEY_LOCATION)
+
+    def update_graph():
+        window.clear()
+
+        new_data_point = youtube.get_dislikes(
+            api_key, settings.YOUTUBE_REWIND_ID)
+        current_datetime = datetime.datetime.now()
+
+        data["data_points"].enqueue(new_data_point)
+        data["time_points"].enqueue(current_datetime.strftime("%I:%M %p"))
+
+        graph.create_graph(
+            window, turtle,
+            data["data_points"].queue, axes=data["time_points"].queue)
+
+        window.ontimer(update_graph, settings.UPDATE_DELAY_SECONDS * 1000)
+
+    update_graph()
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
